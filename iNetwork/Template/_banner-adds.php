30441b1545a0bc47ec3726e8c5dc6be4d419c043
Sourabh Dattaram Sakpal <90976821+SourabhSakpal93@users.noreply.github.com>
2022-06-02 16:44:51 +0530

    Add files via upload

---
+++ iNetwork/Template/_banner-adds.php
@@ -0,0 +1,7 @@
+<!--Banner Ads-->
+<section id="banner_adds">
+    <div class=" py-5 text-color">
+        <img src="./assets/half banner.jpg" alt="banner1" class="img-fluid">
+    </div>
+</section>
+<!--Banner Ads-->

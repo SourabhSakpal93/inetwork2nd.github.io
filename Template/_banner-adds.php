8f241a5aa06f72e1855334fc0a213a5ea2d036e4
Sourabh Dattaram Sakpal <90976821+SourabhSakpal93@users.noreply.github.com>
2022-06-02 16:34:51 +0530

    Delete Template directory

--- Template/_banner-adds.php
+++
@@ -1,7 +0,0 @@
-<!--Banner Ads-->
-<section id="banner_adds">
-    <div class=" py-5 text-color">
-        <img src="./assets/half banner.jpg" alt="banner1" class="img-fluid">
-    </div>
-</section>
-<!--Banner Ads-->

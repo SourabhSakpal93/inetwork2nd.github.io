038a89a8de5192ed316b5063291f9b03e1700106
Sourabh Dattaram Sakpal <90976821+SourabhSakpal93@users.noreply.github.com>
2022-06-02 15:46:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+ob_start();
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+// include banner area file
+include ('Template/_banner-area.php');
+// include banner area file
+
+// include Top sale file
+include ('Template/_top-sale.php');
+// include Top sale file
+
+// include special price file
+include ('Template/_special-price.php');
+// include Special price file
+
+// include Banner adds file
+include ('Template/_banner-adds.php');
+// include Banner adds file
+
+// include Apple Watch file
+include ('Template/_apple-watch.php');
+// include Apple Watch file
+
+// include Blogs file
+include ('Template/_blogs.php');
+// include Blogs file
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,21 @@
+<?php
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+
+// include  product file
+include ('Template/_products.php');
+// include product file
+
+// include Top sale file
+include ('Template/_top-sale.php');
+// include Top sale file
+
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
